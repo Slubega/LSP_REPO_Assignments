72fa5cde22c66be04823f650389dacfe25d39f3c
Slubega <swlubega@gmail.com>
2025-09-09 18:40:30 -0400

    Assignment 1 - Hellow World

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Name: Suleman Lubega
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
